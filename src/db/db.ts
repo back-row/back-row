3f7ff12a35b8494c6c2ff858fb2c5e9f0cf29a67
RobertMili <robertmilicevic3869@gmail.com>
2023-09-28 13:47:02 +0200

    CRUD is almost finish

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, users } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -11,6 +11,78 @@ export async function addAlice() {
   });
   await closeConnection();
 }
+/* later add user:users */
+export async function createUsers(){
+  await prisma.users.create({
+    data: {
+      usersname: 'Alice', 
+      usersemail: 'alice@gmailcom',
+      userspassword: '123456',
+      userstotalscore: null,
+      userslevel: null,
+    }
+  });
+  await closeConnection(); 
+ 
+}
+
+export async function getAllUsers() {
+    const users = await prisma.users.findMany();
+    console.log("This is printing out all users",  users);
+
+}
+
+export async function getUserByID() {
+  const user = await prisma.users.findUnique({
+    where: {
+      usersid: 23,
+    }
+  });
+
+  console.log("This is gonna print out just one user: ", user);
+}
+
+export async function deletUsers() {
+  const users = await prisma.users.deleteMany();
+}
+
+export async function deletUsersById() {
+  const users = await prisma.users.delete({
+    where: {
+      usersid: 2,
+    }
+  })
+}
+
+export async function updateUserByID() {
+  const users = await prisma.users.update({
+      where: {
+        usersid: 23,
+      },
+      data: {
+        usersname: 'Robert',
+      }
+  })
+}
+
+
+// async function getUserByID(tutorialId: number) {
+//   const user = await prisma.tutorial.findUnique({
+//     where: {
+//       tutorialid: tutorialId
+//     }
+//   });
+//   await closeConnection();
+//   return user;
+// }
+
+  
+// (async () => {
+//   const user = await getUserByID(2);
+//   console.log(user);
+
+// })();
+
 
 async function closeConnection() {
   try {
