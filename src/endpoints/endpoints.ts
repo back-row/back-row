a62898fd5ca4c5517d8ba28454089cc3ccd5aba5
RobertMili <robertmilicevic3869@gmail.com>
2023-09-28 18:20:26 +0200

    adding try/catch on CRUD

--- src/endpoints/endpoints.ts
+++ src/endpoints/endpoints.ts
@@ -1,59 +1,66 @@
 import express, { Express, Request, Response } from 'express';
-import { addAlice,createUsers,getAllUsers, getUserByID,updateUserByID} from '../db/db';
-// import { deletUsers} from '../db/db';
+import { createUsers, getAllUsers, getUserByID, updateUserByID } from '../db/db';
 import { deletUsersById } from '../db/db';
 import { userInfo } from 'os';
 
-
 export function endpoints(app: Express) {
-  
+  app.post('/', (req: Request, res: Response) => {
+    try {
+      res.send('BACKROW');
 
-
-  app.get('/', (req: Request, res: Response) => {
-    res.send('BACKROW');
-
-    const data = req.body;
-
-
-    createUsers(data);
-   
+      const data = req.body;
+      createUsers(data);
+    } catch (error) {
+      console.log('Error creating user:', error);
+      res.status(500).json({ error: 'An error occurred while creating user' });
+    }
   });
 
   app.get('/users', (req: Request, res: Response) => {
-    res.send('BACKROW');
-    getAllUsers();
- 
+    try {
+      res.send('BACKROW');
+      getAllUsers();
+    } catch (error) {
+      console.log('Error getting all users:', error);
+      res.status(500).json({ error: 'An error occurred while getting all users' });
+    }
   });
 
-  
   app.get('/users/:id', (req: Request, res: Response) => {
-    const number =  +req.params.id
+    try {
+      const number = +req.params.id;
 
-    res.send('BACKROW');
-    getUserByID(number);
-   
-  // add more endpoints here
-});
+      res.send('BACKROW');
+      getUserByID(number);
+    } catch (error) {
+      console.log('Error getting user by id:', error);
+      res.status(500).json({ error: 'An error occurred while getting user by id' });
+    }
+  });
 
-app.get('/users/delete/:id', (req: Request, res: Response) => {
-  const number =  +req.params.id
+  app.get('/users/delete/:id', (req: Request, res: Response) => {
+    try {
+      const number = +req.params.id;
 
-  res.send('BACKROW');
-  deletUsersById(number);
- 
-// add more endpoints here
-});
+      res.send('BACKROW');
+      deletUsersById(number);
+    } catch (error) {
+      console.log('Error deleting user by id:', error);
+      res.status(500).json({ error: 'An error occurred while deleting user by id' });
+    }
+  });
 
-app.post('/users/update/:id/', (req: Request, res: Response) => {
-  const number = +req.params.id
-  
-  const data = req.body;
+  app.post('/users/update/:id/', (req: Request, res: Response) => {
+    try {
+      const number = +req.params.id;
 
-  
-  res.send('BACKROW');
-  updateUserByID(number, data);
+      const data = req.body;
 
-});
-
-
+      res.send('BACKROW');
+      updateUserByID(number, data);
+    } catch (error) {
+      console.log('Error updating user by id:', error);
+      res.status(500).json({ error: 'An error occurred while updating user by id' });
+    }
+  });
 }
