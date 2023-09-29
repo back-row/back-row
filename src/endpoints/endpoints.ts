12481e7246711f5813d6f2c9f6089fcad850aa0f
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-09-29 13:23:40 +0200

    Update getAllUsers

--- src/endpoints/endpoints.ts
+++ src/endpoints/endpoints.ts
@@ -1,9 +1,6 @@
 import { Express, Request, Response } from 'express';
-import { createUsers, getUserByID, updateUserByID } from '../db/db';
+import { createUsers, getUserByID, getAllUsers,  updateUserByID } from '../db/db';
 import { deletUsersById } from '../db/db';
-import {  PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
 
 export function endpoints(app: Express) {
   app.post('/addUser', (req: Request, res: Response) => {
@@ -29,8 +26,8 @@ export function endpoints(app: Express) {
 
   app.get('/users', async (req: Request, res: Response) => {
     try {
-      const users = await prisma.users.findMany()
-      res.send(users);
+      const users = await getAllUsers()
+      res.json(users);
     } catch (error) {
       console.log('Error getting all users:', error);
       res.status(500).json({ error: 'An error occurred while getting all users' });
