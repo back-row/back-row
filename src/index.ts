2d7e6d55f8a589ceeeeb352274109b7907b9b96c
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-02 14:39:38 +0200

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import express, { Express, Request, Response } from 'express';
+const cors = require('cors');
 import dotenv from 'dotenv';
 import { endpoints } from './endpoints/endpoints';
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: 'POST'
+};
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
 
 endpoints(app);
 
