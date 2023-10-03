5d58cbb8740dc67bfb0d065d832c67d17ec092c8
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-03 14:10:31 +0200

    add getMapLocation

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, users } from '@prisma/client';
+import { PrismaClient, map, users } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -12,60 +12,72 @@ export async function addAlice() {
   await closeConnection();
 }
 
-export async function createUsers(user: users ){
+export async function createUsers(user: users) {
   await prisma.users.create({
     data: {
-      usersname: user.usersname, 
+      usersname: user.usersname,
       usersemail: user.usersemail,
       userspassword: user.userspassword,
       userstotalscore: 0,
-      userslevel: 1,
+      userslevel: 1
     }
   });
-  await closeConnection(); 
- 
+  await closeConnection();
 }
 
 export async function getAllUsers() {
-    const users = await prisma.users.findMany();
-    console.log("This is printing out all users",  users);
-    return users
+  const users = await prisma.users.findMany();
+  console.log('This is printing out all users', users);
+  return users;
 }
 
 export async function getUserByID(id: number) {
   const user = await prisma.users.findUnique({
     where: {
-      usersid: id,
+      usersid: id
     }
   });
-   
-  return user
-}
 
+  return user;
+}
 
 export async function deletUsersById(id: number) {
   const users = await prisma.users.delete({
     where: {
-      usersid: id,
+      usersid: id
     }
-  })
-  await closeConnection(); 
+  });
+  await closeConnection();
 }
 
 export async function updateUserByID(id: number, data: users) {
   const users = await prisma.users.update({
-      where: {
-        usersid: id,
-      },
-      data: {
-        usersname: data.usersname, 
-    
-      }
-  })
-  
-  await closeConnection(); 
+    where: {
+      usersid: id
+    },
+    data: {
+      usersname: data.usersname
+    }
+  });
+
+  await closeConnection();
 }
 
+export async function getMapLocations(id: number) {
+  let maplocations = { rows: 1, column: 1 };
+  try {
+    let map = await prisma.map.findUnique({ where: { mapid: id } });
+
+    if (map != null && map.mapendlocationrow != null && map.mapendlocationcolumn != null) {
+      maplocations.rows = map.mapendlocationrow;
+      maplocations.column = map.mapendlocationcolumn;
+    }
+    return maplocations;
+  } catch (e) {
+    console.log(e);
+    return maplocations;
+  }
+}
 
 async function closeConnection() {
   try {
