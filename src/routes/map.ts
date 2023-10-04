3a4cae218a765500015bd92d98f2b8cc9b1980bc
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-04 10:18:03 +0200

    add map endpoint

---
+++ src/routes/map.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { getMap } from '../db/db';
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/:id', async (req: Request, res: Response) => {
+  const id = +req.params.id;
+
+  try {
+    const map = await getMap(id);
+    res.json(map);
+  } catch (e) {
+    console.log(e);
+    res.status(404).send('Map not found');
+  }
+});
+
+export default router;
