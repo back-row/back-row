87dadd454d38c71594a912d92a4deaa793482eaa
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-04 11:16:03 +0200

    Add tutorial endpoint

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,7 +3,7 @@ import { PrismaClient, users } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function addAlice() {
-  await prisma.tutorial.create({
+  await prisma.map.create({
     data: {
       tutorialdescription: 'Alice',
       tutorialhint: 'test@tst.es'
@@ -12,60 +12,70 @@ export async function addAlice() {
   await closeConnection();
 }
 
-export async function createUsers(user: users ){
+export async function createUsers(user: users) {
   await prisma.users.create({
     data: {
-      usersname: user.usersname, 
+      usersname: user.usersname,
       usersemail: user.usersemail,
       userspassword: user.userspassword,
       userstotalscore: 0,
-      userslevel: 1,
+      userslevel: 1
     }
   });
-  await closeConnection(); 
- 
+  await closeConnection();
 }
 
 export async function getAllUsers() {
-    const users = await prisma.users.findMany();
-    console.log("This is printing out all users",  users);
-    return users
+  const users = await prisma.users.findMany();
+  console.log('This is printing out all users', users);
+  return users;
 }
 
 export async function getUserByID(id: number) {
   const user = await prisma.users.findUnique({
     where: {
-      usersid: id,
+      usersid: id
     }
   });
-   
-  return user
-}
 
+  return user;
+}
 
 export async function deletUsersById(id: number) {
   const users = await prisma.users.delete({
     where: {
-      usersid: id,
+      usersid: id
     }
-  })
-  await closeConnection(); 
+  });
+  await closeConnection();
 }
 
 export async function updateUserByID(id: number, data: users) {
   const users = await prisma.users.update({
-      where: {
-        usersid: id,
-      },
-      data: {
-        usersname: data.usersname, 
-    
-      }
-  })
-  
-  await closeConnection(); 
+    where: {
+      usersid: id
+    },
+    data: {
+      usersname: data.usersname
+    }
+  });
+
+  await closeConnection();
 }
 
+export async function getTutorialByMapId(id: number) {
+  console.log('Getting tutorial by mapid: ' + id);
+  const map = await prisma.map.findUnique({
+    where: {
+      mapid: id
+    },
+    include: {
+      tutorial: true
+    }
+  });
+  await closeConnection();
+  return map?.tutorial;
+}
 
 async function closeConnection() {
   try {
--- src/routes/tutorial.ts
+++ src/routes/tutorial.ts
@@ -1,10 +1,24 @@
 import { Request, Response } from 'express';
+import { getTutorialByMapId } from '../db/db';
 
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.status(200).end();
+router.get('/:id', async (req: Request, res: Response) => {
+  const id = +req.params.id;
+
+  console.log('New request for tutorial by mapid: ' + id + ' received.');
+  try {
+    const tutorial = await getTutorialByMapId(id);
+    if (!tutorial) {
+      res.status(404).send('Tutorial not found').end();
+      return;
+    }
+    res.json(tutorial).end();
+  } catch (e) {
+    console.log(e);
+    res.status(500).send('Internal Server Error').end();
+  }
 });
 
 export default router;
