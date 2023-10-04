b0ad4500c31598728444035bee35656e5dbf69da
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-10-04 14:31:19 +0200

    Add getQuestion to quiz endpoint

---
+++ sql/quiz.sql
@@ -0,0 +1,3 @@
+INSERT INTO quiz (quizQuestion, quizAnswer1, quizAnswer2, quizAnswer3, quizCorrectAnswer)
+VALUES
+  ('Vilket nyckelord används för att deklarera konstanter i JavaScript/TypeScript?', 'a) var', 'b) let', 'c) const', 3);
