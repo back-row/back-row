c5e3e33c612ec4f1990f4b3bf518af4b7bd8e21a
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-10-06 15:00:29 +0200

    Update zmap.sql with tutorialId

--- sql/zmap.sql
+++ sql/zmap.sql
@@ -1,3 +1,3 @@
-INSERT INTO map (mapEndLocationColumn, mapEndLocationRow, mapQuizId)
+INSERT INTO map (mapEndLocationColumn, mapEndLocationRow, mapQuizId, mapTutorialId)
 VALUES
-  (3, 7, (SELECT quizid from quiz WHERE quizid=1));
+  (3, 7, (SELECT quizid from quiz WHERE quizid=1), (SELECT tutorialId from tutorial WHERE tutorialId=1));
