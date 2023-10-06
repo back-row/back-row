512ae59852addf935afb64ba55d7e89e27aa5bfd
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-10-06 09:36:22 +0200

    Update quiz 1 move commands

--- sql/quiz.sql
+++ sql/quiz.sql
@@ -1,4 +1,4 @@
 INSERT INTO quiz (quizQuestion)
 VALUES
-  ('{"question": "Vilket nyckelord används för att deklarera konstanter i JavaScript/TypeScript?", "choice1": "var", "answer1": ["up", "right"], "choice2":"let","answer2":["down","up"], "choice3":"const", "answer3":["right","down"]}');
+  ('{"question": "Vilket nyckelord används för att deklarera konstanter i JavaScript/TypeScript?", "choice1": "var", "answer1": ["down","down","down","down","down","right","right","right","up","left"], "choice2":"let","answer2":["down","up","right","right","right","right","down","left"], "choice3":"const", "answer3":["right","down","right","down","down", "down","down", "down"]}');
 
