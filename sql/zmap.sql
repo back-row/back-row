9b40ee14dcf20832b02cc7f082f3d86a23839aa2
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-12 11:50:10 +0200

    add admin and more maps

--- sql/zmap.sql
+++ sql/zmap.sql
@@ -1,3 +1,13 @@
 INSERT INTO map (mapEndLocationx, mapEndLocationy, mapQuizId, mapTutorialId)
 VALUES
   (230, 400, (SELECT quizid from quiz WHERE quizid=1), (SELECT tutorialId from tutorial WHERE tutorialId=1));
+  
+
+INSERT INTO map (mapEndLocationx, mapEndLocationy, mapQuizId, mapTutorialId)
+VALUES
+  (400, 400, (SELECT quizid from quiz WHERE quizid=1), (SELECT tutorialId from tutorial WHERE tutorialId=1));
+
+
+  INSERT INTO map (mapEndLocationx, mapEndLocationy, mapQuizId, mapTutorialId)
+VALUES
+  (600, 300, (SELECT quizid from quiz WHERE quizid=1), (SELECT tutorialId from tutorial WHERE tutorialId=1));
