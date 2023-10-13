5e9fc2e698eee9c7af08e73fd54eea35a733ba7c
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-13 11:18:47 +0200

    add post endpoint

--- src/routes/score.ts
+++ src/routes/score.ts
@@ -4,6 +4,20 @@ import { authenticateToken } from '../endpoints/auth';
 const express = require('express');
 const router = express.Router();
 
-router.post('/', authenticateToken, async (req: Request, res: Response) => {});
+router.post('/', authenticateToken, async (req: Request, res: Response) => {
+  const user = req.user.userid;
+  const map = req.body.map;
+  const score = req.body.score;
 
+  const userScore = await getUserScore(user, map);
+
+  if (!userScore) {
+    await insertScore(user, map, score);
+    res.status(201).send('Score inserted').end();
+  } else if (userScore.score < score) {
+    await updateScore(user, map, score);
+    res.status(200).send('Score updated').end();
+  }
+  res.status(200).send('Score not updated').end();
+});
 export default router;
