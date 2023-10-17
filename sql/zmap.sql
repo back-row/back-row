23ef3038659babaa16d52bfe3137cd615e7d18fe
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-17 14:33:59 +0200

    add map2 to sql file

--- sql/zmap.sql
+++ sql/zmap.sql
@@ -4,7 +4,7 @@ VALUES
 
 INSERT INTO map (mapEndLocationx, mapEndLocationy, mapstartlocationx, mapstartlocationy, mapJSON, mapQuizId, mapTutorialId)
 VALUES
-  (230, 400, 80, 90, 'mapOne', (SELECT quizid from quiz WHERE quizid=1), (SELECT tutorialId from tutorial WHERE tutorialId=1));
+  (40, 525, 40, 90, 'mapTwo', (SELECT quizid from quiz WHERE quizid=1), (SELECT tutorialId from tutorial WHERE tutorialId=1));
 
 INSERT INTO map (mapEndLocationx, mapEndLocationy, mapstartlocationx, mapstartlocationy, mapJSON, mapQuizId, mapTutorialId)
 VALUES
