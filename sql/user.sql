6ec126c25b8b8f7af626096ee6aea60a6a7aa0c4
Oliver Säfström <safstrom.oliver@gmail.com>
2023-10-18 15:43:26 +0200

    add map5 quiz and map

--- sql/user.sql
+++ sql/user.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (usersname, userspassword, usersemail, userstotalscore, usersimage, userslevel)
 VALUES
-  ('admin', '12345', 'admin@admin.com', 200, 'boy1', 2);
+  ('admin', '12345', 'admin@admin.com', 200, 'boy1', 100);
--- sql/zmap.sql
+++ sql/zmap.sql
@@ -9,3 +9,11 @@ VALUES
 INSERT INTO map (mapEndLocationx, mapEndLocationy, mapstartlocationx, mapstartlocationy, mapJSON, mapQuizId, mapTutorialId)
 VALUES
   (230, 400, 80, 90, 'mapOne', (SELECT quizid from quiz WHERE quizid=1), (SELECT tutorialId from tutorial WHERE tutorialId=1));
+
+INSERT INTO map (mapEndLocationx, mapEndLocationy, mapstartlocationx, mapstartlocationy, mapJSON, mapQuizId, mapTutorialId)
+VALUES
+  (230, 400, 80, 90, 'mapOne', (SELECT quizid from quiz WHERE quizid=1), (SELECT tutorialId from tutorial WHERE tutorialId=1));
+
+INSERT INTO map (mapEndLocationx, mapEndLocationy, mapstartlocationx, mapstartlocationy, mapJSON, mapQuizId, mapTutorialId)
+VALUES
+  (695, 280, 71, 291, 'mapFive', (SELECT quizid from quiz WHERE quizid=5), (SELECT tutorialId from tutorial WHERE tutorialId=5));
