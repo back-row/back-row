97359cf455ed9cf0cd177ebe1246c069cd3b786d
RobertMili <robertmilicevic3869@gmail.com>
2023-10-22 10:21:37 +0200

    update quiz (treasure )

--- sql/quiz.sql
+++ sql/quiz.sql
@@ -34,7 +34,7 @@ VALUES
        );
   INSERT INTO quiz (quizQuestion)
 VALUES
-  ('{"question": "Which combination of commands makes the player move to the treasure chest?", "choice1": "down , down, down, down, down, right , right , right , up , left", "answer1": ["down","down","down","down","down","right","right","right","up","left"], "choice2":"down, up, right, right, right, right, down, left","answer2":["down","up","right","right","right","right","down","left"], "choice3":"right, down, right, down, down, down, down, down", "answer3":["right","down","right","down","down", "down","down", "down"]}');
+  ('{"question": "Which combination of commands makes the player move to the treasure princess?", "choice1": "down , down, down, down, down, right , right , right , up , left", "answer1": ["down","down","down","down","down","right","right","right","up","left"], "choice2":"down, up, right, right, right, right, down, left","answer2":["down","up","right","right","right","right","down","left"], "choice3":"right, down, right, down, down, down, down, down", "answer3":["right","down","right","down","down", "down","down", "down"]}');
 
  INSERT INTO quiz (quizQuestion)
 VALUES
