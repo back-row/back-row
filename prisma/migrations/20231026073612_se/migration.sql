38bc64d40cbd255848eda6caf4ccc797363f8d57
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-10-26 09:38:04 +0200

    prisma migration

---
+++ prisma/migrations/20231026073612_se/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "quiz" ADD COLUMN     "quizquestionse" TEXT;
+
+-- AlterTable
+ALTER TABLE "tutorial" ADD COLUMN     "tutorialdescriptionse" TEXT,
+ADD COLUMN     "tutorialhintse" TEXT;
