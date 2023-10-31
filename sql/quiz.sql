1ab06b606790dbcc3912908d308b309d7258dd7b
jLereback <112405931+jLereback@users.noreply.github.com>
2023-10-31 14:06:26 +0100

    Add javascript questions

--- sql/quiz.sql
+++ sql/quiz.sql
@@ -1,81 +1,97 @@
-INSERT INTO quiz (quizQuestion,quizQuestionSe)
-VALUES
-    ('{
-    "question": "Which combination of commands makes the player move to the princess?",
-    "choice1": "down, down, right, right, up",
+INSERT INTO quiz (quizQuestion, quizQuestionSe)
+VALUES ('{
+    "question": "Which keyword is used to declare variables in JavaScript?",
+    "choice1": "variable",
     "answer1": ["down", "down", "right", "right", "up"],
-    "choice2": "right, right, right, down, down, down, down, left, left",
+    "choice2": "var",
     "answer2": ["right", "right", "right", "down", "down", "down", "down", "left", "left"],
-    "choice3": "right, right, down, right, right, down, up, right, right, right",
+    "choice3": "let",
     "answer3": ["right", "right", "down", "right", "right", "down", "up", "right", "right", "right"]
   }',
-  '{
-    "question": "Vilka kommandon får spelaren att gå till prinsessan?",
-    "choice1": "down, down, right, right, up",
-    "answer1": ["down", "down", "right", "right", "up"],
-    "choice2": "right, right, right, down, down, down, down, left, left",
-    "answer2": ["right", "right", "right", "down", "down", "down", "down", "left", "left"],
-    "choice3": "right, right, down, right, right, down, up, right, right, right",
-    "answer3": ["right", "right", "down", "right", "right", "down", "up", "right", "right", "right"]
-  }');
+        '{
+          "question": "Vilket nyckelord används för att deklarera variabler i JavaScript?",
+          "choice1": "variable",
+          "answer1": ["down", "down", "right", "right", "up"],
+          "choice2": "var",
+          "answer2": ["right", "right", "right", "down", "down", "down", "down", "left", "left"],
+          "choice3": "let",
+          "answer3": ["right", "right", "down", "right", "right", "down", "up", "right", "right", "right"]
+        }');
 
-  INSERT INTO quiz (quizQuestion, quizQuestionSe)
-VALUES
-  ('{"question": "Which combination of commands makes the player move to the princess?",
-      "choice1": "down , down, down, down, down, right and so one repeating  ",
+INSERT INTO quiz (quizQuestion, quizQuestionSe)
+VALUES ('{"question": "What is ''document.getElementById()'' used for in JavaScript?",
+      "choice1": "To create a new HTML element",
       "answer1": ["down","down","down","down","down","right","right","right","up","left"],
-      "choice2":"down, up, right, right, right, right, down,up  and so one repeating ",
-      "answer2":["down","up","right","right","right","right","down","left"],
-      "choice3":"down, down, right, right, up, up, right and so one repeating ",
-      "answer3":["down", "down", "right", "right", "up", "up", "right", "right", "down", "down", "right", "right", "up", "up", "right", "right", "down", "down", "down", "down", "down", "left", "left", "up", "up", "left", "left", "down", "down", "left", "left", "up", "up", "left", "left", "down", "down"]}',
-      '{"question": "Vilka kommandon får spelaren att gå till prinsessan?",
-      "choice1": "down , down, down, down, down, right and so one repeating  ",
-      "answer1": ["down","down","down","down","down","right","right","right","up","left"],
-      "choice2":"down, up, right, right, right, right, down,up  and so one repeating ",
-      "answer2":["down","up","right","right","right","right","down","left"],
-      "choice3":"down, down, right, right, up, up, right and so one repeating ",
-      "answer3":["down", "down", "right", "right", "up", "up", "right", "right", "down", "down", "right", "right", "up", "up", "right", "right", "down", "down", "down", "down", "down", "left", "left", "up", "up", "left", "left", "down", "down", "left", "left", "up", "up", "left", "left", "down", "down"]}');
+      "choice2": "To remove an HTML element",
+      "answer2": ["down","up","right","right","right","right","down","left"],
+      "choice3": "To retrieve an HTML element with a specific id",
+      "answer3": ["down", "down", "right", "right", "up", "up", "right", "right", "down", "down", "right", "right", "up", "up", "right", "right", "down", "down", "down", "down", "down", "left", "left", "up", "up", "left", "left", "down", "down", "left", "left", "up", "up", "left", "left", "down", "down"]' ||
+        '}',
+        '{"question": "Vad används ''document.getElementById()'' för i JavaScript?",
+           "choice1": "För att skapa ett nytt HTML-element",
+           "answer1": ["down","down","down","down","down","right","right","right","up","left"],
+           "choice2": "För att ta bort ett HTML-element",
+           "answer2": ["down","up","right","right","right","right","down","left"],
+           "choice3": "För att hämta ett HTML-element med ett specifikt id",
+           "answer3": ["down", "down", "right", "right", "up", "up", "right", "right", "down", "down", "right", "right", "up", "up", "right", "right", "down", "down", "down", "down", "down", "left", "left", "up", "up", "left", "left", "down", "down", "left", "left", "up", "up", "left", "left", "down", "down"]}');
 
-  INSERT INTO quiz (quizQuestion,quizQuestionSe)
-VALUES
-   ('{"question": "Which combination of commands makes the player move to the princess?",
-      "choice1": "down, down, right, up, right, left, up, right, right, up, up, right  ",
+INSERT INTO quiz (quizQuestion, quizQuestionSe)
+VALUES ('{"question": "Which of the following is not a JavaScript framework?",
+      "choice1": "Angular",
       "answer1": ["down", "down", "right", "up", "left", "right", "up", "right", "right", "up","up","right"],
-      "choice2":"down, down, right, right, right, right, up, right, right, up, up, right",
+      "choice2": "React",
       "answer2": ["down", "down", "right", "right", "right", "right", "up", "right", "right", "up","up","right"] ,
-      "choice3":"down, down, right, right, up, right, right, right, right, up, up, right",
+      "choice3": "Sass",
        "answer3": ["down", "down", "right", "right", "up", "right", "right", "right", "right", "up","up","right"] 
        }',
-       '{"question": "Vilka kommandon får spelaren att gå till prinsessan?",
-      "choice1": "down, down, right, up, right, left, up, right, right, up, up, right  ",
-      "answer1": ["down", "down", "right", "up", "left", "right", "up", "right", "right", "up","up","right"],
-      "choice2":"down, down, right, right, right, right, up, right, right, up, up, right",
-      "answer2": ["down", "down", "right", "right", "right", "right", "up", "right", "right", "up","up","right"] ,
-      "choice3":"down, down, right, right, up, right, right, right, right, up, up, right",
-       "answer3": ["down", "down", "right", "right", "up", "right", "right", "right", "right", "up","up","right"] 
-       }');
-  INSERT INTO quiz (quizQuestion,quizQuestionSe)
-VALUES
-  ('{"question": "Which combination of commands makes the player rescue the princess?", "choice1": "down , down, down, down, down, right , right , right , up , left", "answer1": ["down","down","down","down","down","right","right","right","up","left"], "choice2":"down, up, right, right, right, right, down, left","answer2":["down","up","right","right","right","right","down","left"], "choice3":"right, down, right, down, down, down, down, down", "answer3":["right","down","right","down","down", "down","down", "down"]}',
-  '{"question": "Vilka kommandon får spelaren att rädda prinsessan?", "choice1": "down , down, down, down, down, right , right , right , up , left", "answer1": ["down","down","down","down","down","right","right","right","up","left"], "choice2":"down, up, right, right, right, right, down, left","answer2":["down","up","right","right","right","right","down","left"], "choice3":"right, down, right, down, down, down, down, down", "answer3":["right","down","right","down","down", "down","down", "down"]}');
+        '{"question": "Vilket av följande är inte en JavaScript-ramverk?",
+       "choice1": "Angular",
+       "answer1": ["down", "down", "right", "up", "left", "right", "up", "right", "right", "up","up","right"],
+       "choice2": "React",
+       "answer2": ["down", "down", "right", "right", "right", "right", "up", "right", "right", "up","up","right"] ,
+       "choice3": "Sass",
+        "answer3": ["down", "down", "right", "right", "up", "right", "right", "right", "right", "up","up","right"]
+        }');
+INSERT INTO quiz (quizQuestion, quizQuestionSe)
+VALUES ('{"question": "Which function is used to add event listeners in JavaScript?",
+   "choice1": "addEventListener()",
+  "answer1": ["up","right","right", "coin += 1", "left", "left", "left", "left", "coin += 1", "up",
+  "up", "coin += 1", "down", "down", "right", "right", "right", "right", "coin += 1", "right", "up", "up",
+  "coin += 1", "down", "down", "left", "left", "up", "up", "bribe()", "down", "up", "up", "up"],
+   "choice2": "attachListener()",
+   "answer2": ["down","up","right","right","right","right","down","left"],
+   "choice3": "onEvent()",
+   "answer3":["right","down","right","down","down", "down","down", "down"]
+   }',
+        '{
+        "question": "Vilken funktion används för att lägga till händelselyssnare i JavaScript?",
+        "choice1": "addEventListener()",
+        "answer1": ["up","right","right", "coin += 1", "left", "left", "left", "left", "coin += 1", "up",
+        "up", "coin += 1", "down", "down", "right", "right", "right", "right", "coin += 1", "right", "up", "up",
+        "coin += 1", "down", "down", "left", "left", "up", "up", "bribe()", "down", "up", "up", "up"],
+        "choice2": "attachListener()",
+        "answer2": ["down","up","right","right","right","right","down","left"],
+        "choice3": "onEvent()",
+        "answer3": ["right","down","right","down","down", "down","down", "down"]
+        }');
 
- INSERT INTO quiz (quizQuestion,quizQuestionSe)
-VALUES
-  ('{
-    "question": "Which combination of commands makes the player move to the princess?",
-    "choice1": "down, hide(), right, right, up",
+
+INSERT INTO quiz (quizQuestion, quizQuestionSe)
+VALUES ('{
+    "question": "Which function is used to print to the console in JavaScript?",
+    "choice1": "console.print()",
     "answer1": ["down", "hide()", "right", "right", "up"],
-    "choice2": "hide(), right, right, right, right",
+    "choice2": "console.write()",
     "answer2": ["hide()", "right", "right", "right", "right"],
-    "choice3": "right, right, hide(spikes), right, right, down, up, right, right, right",
+    "choice3": "console.log()",
     "answer3": ["right", "right", "hide(spikes)", "right", "right", "down", "up", "right", "right", "right"]
   }',
-  '{
-    "question": "Vilka kommandon får spelaren att ta sig till prinsessan?",
-    "choice1": "down, hide(), right, right, up",
-    "answer1": ["down", "hide()", "right", "right", "up"],
-    "choice2": "hide(), right, right, right, right",
-    "answer2": ["hide()", "right", "right", "right", "right"],
-    "choice3": "right, right, hide(spikes), right, right, down, up, right, right, right",
-    "answer3": ["right", "right", "hide(spikes)", "right", "right", "down", "up", "right", "right", "right"]
-  }');
+        '{
+          "question": "Vilken funktion används för att skriva ut i konsolen i JavaScript?",
+          "choice1": "console.print()",
+          "answer1": ["down", "hide()", "right", "right", "up"],
+          "choice2": "console.write()",
+          "answer2": ["hide()", "right", "right", "right", "right"],
+          "choice3": "console.log()",
+          "answer3": ["right", "right", "hide(spikes)", "right", "right", "down", "up", "right", "right", "right"]
+        }');
